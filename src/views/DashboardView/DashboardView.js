17839a0ec284fb0b99d93698344cbe2893d0d214
Krystian Pietron <krystianpietr@gmail.com>
2018-11-21 16:35:09 +0100

    add and end feature/13-makieta

---
+++ src/views/DashboardView/DashboardView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import MenuItem from 'material-ui/MenuItem';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+
+const Dashboard = (props) => (
+  <div>
+     <h1> Dashboard</h1>
+  </div>
+)
+export default Dashboard
---
+++ src/views/FavouriteList/FavouriteList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const FavouriteList = () => (
+    <div>
+       <h1>Favourite List</h1>
+    </div>
+)
+export default FavouriteList
