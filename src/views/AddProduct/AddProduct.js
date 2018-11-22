2670652da16b48a172287d9c8c90fe99255c202f
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-22 15:54:19 +0100

    Selected option - works

--- src/views/AddProduct/AddProduct.js
+++ src/views/AddProduct/AddProduct.js
@@ -7,41 +7,38 @@ const categories = ["Vegetables", "Fruits", "Drinks", "Meat", "Other"];
 
 class AddProduct extends React.Component {
   state = {
-      values: []
-  }
+    value: null
+  };
 
-  handleChange = (event, index, values) => this.setState({categories})
+  handleChange = (event, index, value) => this.setState({ value: value });
 
   menuItems(values) {
-      return categories.map((category) => (
-          <MenuItem
-            key={category}
-            insetChildren={true}
-            checked={values && values.indexOf(category) > -1}
-            value={category}
-            primaryText={category}
-          />
-      ))
+    return;
   }
+
   render() {
-      const {values} = this.state
     return (
       <div>
         <h1>Add product</h1>
         <div>
-          <TextField
-             hintText="Product name"
-         />
+          <TextField hintText="Product name" />
         </div>
         <div>
-            <SelectField
-                multiple={true}
-                hintText="Choose category"
-                value={values}
-                onChange={this.handleChange}
-            >
-            {this.menuItems(values)}
-            </SelectField>
+          <SelectField
+            // multiple={true}
+            hintText="Choose category"
+            value={this.state.value}
+            onChange={this.handleChange}
+          >
+            {categories.map(category => (
+              <MenuItem
+                key={category}
+                insetChildren={true}
+                value={category}
+                primaryText={category}
+              />
+            ))}
+          </SelectField>
         </div>
       </div>
     );
