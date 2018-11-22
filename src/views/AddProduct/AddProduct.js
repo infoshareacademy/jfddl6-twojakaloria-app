e96cebae70ef99bfa3f7ce769add0e8ec73449a9
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-22 16:16:18 +0100

    Started work with react-flexbox-grid

--- src/views/AddProduct/AddProduct.js
+++ src/views/AddProduct/AddProduct.js
@@ -2,6 +2,7 @@ import React from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const categories = ["Vegetables", "Fruits", "Drinks", "Meat", "Other"];
 
@@ -12,47 +13,58 @@ class AddProduct extends React.Component {
 
   handleChange = (event, index, value) => this.setState({ value: value });
 
-  menuItems(values) {
-    return;
-  }
+
 
   render() {
     return (
       <div>
-        <h1>Add product</h1>
-        <div>
-          <TextField hintText="Product name" />
+          <Grid fluid>
+            <Row center="xs">
+                <Col lg={1}/>
+                <h1>Add product</h1>
+            </Row>
+            <Row center="xs">
+                    <Col lg={1}/>
+                    <div>
+                    <TextField hintText="Product name" />
+                    </div>
+                    <Col lg={1}/>
+                    <div>
+                    <TextField hintText="Kcal" />
+                    </div>
+                    <Col lg={1}/>
+                    <div>
+                    <TextField hintText="Protein" />
+                    </div>
+                    <Col lg={1}/>
+                    <div>
+                    <TextField hintText="Fat" />
+                    </div>
+                    <Col lg={1}/>
+                    <div>
+                    <TextField hintText="Carbohydrates" />
+                    </div>
+                    <Col lg={1}/>
+                    <div>
+                    <SelectField
+                        // multiple={true}
+                        hintText="Choose category"
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                    >
+                        {categories.map(category => (
+                        <MenuItem
+                            key={category}
+                            insetChildren={true}
+                            value={category}
+                            primaryText={category}
+                        />
+                        ))}
+                    </SelectField>
+                    </div>
+            </Row>
+            </Grid>
         </div>
-        <div>
-          <TextField hintText="Kcal" />
-        </div>
-        <div>
-          <TextField hintText="Protein" />
-        </div>
-        <div>
-          <TextField hintText="Fat" />
-        </div>
-        <div>
-          <TextField hintText="Carbohydrates" />
-        </div>
-        <div>
-          <SelectField
-            // multiple={true}
-            hintText="Choose category"
-            value={this.state.value}
-            onChange={this.handleChange}
-          >
-            {categories.map(category => (
-              <MenuItem
-                key={category}
-                insetChildren={true}
-                value={category}
-                primaryText={category}
-              />
-            ))}
-          </SelectField>
-        </div>
-      </div>
     );
   }
 }
