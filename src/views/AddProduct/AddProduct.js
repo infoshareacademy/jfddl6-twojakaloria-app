cb0e1caae7ef32dce4e0403c2b2f7d996a3f714f
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-22 16:51:48 +0100

    Started working with materialize Paper

--- src/views/AddProduct/AddProduct.js
+++ src/views/AddProduct/AddProduct.js
@@ -2,7 +2,8 @@ import React from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import Paper from "material-ui/Paper";
+import { Grid, Row, Col } from "react-flexbox-grid";
 
 const categories = ["Vegetables", "Fruits", "Drinks", "Meat", "Other"];
 
@@ -13,58 +14,62 @@ class AddProduct extends React.Component {
 
   handleChange = (event, index, value) => this.setState({ value: value });
 
-
-
   render() {
     return (
       <div>
-          <Grid fluid>
-            <Row center="xs">
-                <Col lg={1}/>
-                <h1>Add product</h1>
-            </Row>
-            <Row center="xs">
-                    <Col lg={1}/>
-                    <div>
-                    <TextField hintText="Product name" />
+        <Grid fluid>
+          <Row center="xs">
+            <Col lg={12} />
+            <h1>Add product</h1>
+          </Row>
+          <div>
+          <Paper>
+              <div>
+              <Row>
+                <Col lg={12} />
+                <TextField
+                    hintText="Product name"
+                 />
+              </Row>
                     </div>
-                    <Col lg={1}/>
-                    <div>
-                    <TextField hintText="Kcal" />
-                    </div>
-                    <Col lg={1}/>
-                    <div>
-                    <TextField hintText="Protein" />
-                    </div>
-                    <Col lg={1}/>
-                    <div>
-                    <TextField hintText="Fat" />
-                    </div>
-                    <Col lg={1}/>
-                    <div>
-                    <TextField hintText="Carbohydrates" />
-                    </div>
-                    <Col lg={1}/>
-                    <div>
-                    <SelectField
-                        // multiple={true}
-                        hintText="Choose category"
-                        value={this.state.value}
-                        onChange={this.handleChange}
-                    >
-                        {categories.map(category => (
-                        <MenuItem
-                            key={category}
-                            insetChildren={true}
-                            value={category}
-                            primaryText={category}
-                        />
-                        ))}
-                    </SelectField>
-                    </div>
-            </Row>
-            </Grid>
-        </div>
+              <Row center="xs">
+                <Col lg={12} />
+                <TextField hintText="Product name" />
+              </Row>
+              <Row center="xs">
+                <Col lg={12} />
+                <TextField hintText="Product name" />
+              </Row>
+              <Row center="xs">
+                <Col lg={12} />
+                <TextField hintText="Product name" />
+              </Row>
+              <Row center="xs">
+                <Col lg={12} />
+                <TextField hintText="Product name" />
+              </Row>
+              <Row center="xs">
+                <Col lg={12} />
+                <SelectField
+                  // multiple={true}
+                  hintText="Choose category"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                >
+                  {categories.map(category => (
+                    <MenuItem
+                      key={category}
+                      insetChildren={true}
+                      value={category}
+                      primaryText={category}
+                    />
+                  ))}
+                </SelectField>
+              </Row>
+          </Paper>
+            </div>
+        </Grid>
+      </div>
     );
   }
 }
