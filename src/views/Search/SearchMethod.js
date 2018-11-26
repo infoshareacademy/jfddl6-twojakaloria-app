86d5982689747ebec61e4f0264661363f8dfeeb3
Krystian Pietron <krystianpietr@gmail.com>
2018-11-26 16:57:52 +0100

    add name filter for method search

--- src/views/Search/SearchMethod.js
+++ src/views/Search/SearchMethod.js
@@ -4,36 +4,38 @@ import ActionFavorite from 'material-ui/svg-icons/action/favorite';
 import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
 import IconButton from 'material-ui/IconButton'
 import DeleteIcon from 'material-ui/svg-icons/action/delete'
-
 import data from '../ProductList/Product'
+import { unifyString } from './unify'
 
 const SearchMethod = (props) => (
     data
-        .filter((product) => product.name.toLowerCase(props.name).indexOf(props.name) !== -1)
-        .map((product) => (
-            <ListItem
-                key={product.name}
-                primaryText={product.name}
-                rightIconButton={
-                    <div>
-                        <IconButton>
-                            <DeleteIcon
-                            // onClick={() => props.deleteTask(props.task.key)}
-                            />
-                        </IconButton>
-                        <IconButton>
-                            {product.favorites===true?
-                            <ActionFavorite
-                            onClick={()=>{}}
-                            />
-                            :
-                            <ActionFavoriteBorder 
-                            onClick={()=>{}}/>}
-                        </IconButton>
-                    </div>
-                }
-            />
-        ))
-)
+        .filter((product) => unifyString(product.name)
+            .includes(
+                unifyString(props.name)))
+            .map((product) => (
+                <ListItem
+                    key={product.name}
+                    primaryText={product.name}
+                    rightIconButton={
+                        <div>
+                            <IconButton>
+                                <DeleteIcon
+                                // onClick={() => props.deleteTask(props.task.key)}
+                                />
+                            </IconButton>
+                            <IconButton>
+                                {product.favorites === true ?
+                                    <ActionFavorite
+                                        onClick={() => { }}
+                                    />
+                                    :
+                                    <ActionFavoriteBorder
+                                        onClick={() => { }} />}
+                            </IconButton>
+                        </div>
+                    }
+                />
+            ))
+        )
 
 export default SearchMethod
