633a4a4c27dbe4df61c07693b550cd629ee73e95
Krystian Pietron <krystianpietr@gmail.com>
2018-11-27 20:31:56 +0100

    add method deleta and favourite for icons to search

--- src/views/Search/SearchTaskForName.js
+++ src/views/Search/SearchTaskForName.js
@@ -4,6 +4,7 @@ import ActionFavorite from 'material-ui/svg-icons/action/favorite';
 import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
 import IconButton from 'material-ui/IconButton'
 import DeleteIcon from 'material-ui/svg-icons/action/delete'
+import Checkbox from 'material-ui/Checkbox';
 
 
 const API_URL = 'https://twoja-kaloria.firebaseio.com/products'
@@ -30,41 +31,47 @@ class Task extends React.Component {
                 this.setState({ tasks: tasksList })
             })
     }
-    deleteHandler(id){
-        fetch(`${API_URL}/${id}.json`,{
-            method: 'DELETE'})
-            .then(this.loadData())
+    deleteHandler(id) {
+        fetch(`${API_URL}/${id}.json`, {
+            method: 'DELETE'
+        }).then(this.loadData())
+    }
+    isFavourite(task) {
+        fetch(`${API_URL}/${task.id}/.json`, {
+            method: 'PATCH',
+            body: JSON.stringify({ isFavourite: !task.isFavourite })
+        }
+        ).then(this.loadData())
     }
     render() {
         return (
-            <div>
-                {console.log(this.state.tasks)}
-                {this.state.tasks.map((product) => (
-                    <ListItem
-                        key={product.name}
-                        primaryText={product.name}
-                        rightIconButton={
-                            <div>
-                                <IconButton>
-                                    <DeleteIcon
-                                     onClick={() => this.deleteHandler(product.id)}
-                                    />
-                                </IconButton>
-                                <IconButton>
-                                    {product.favorites === true ?
-                                        <ActionFavorite
-                                            onClick={() =>{} }
-                                        />
-                                        :
-                                        <ActionFavoriteBorder
-                                            onClick={() => { }} />}
-                                </IconButton>
-                            </div>
-                        }
-                    />
-                ))
-                }
-            </div>)
+
+            this.state.tasks.map((product) => (
+                <ListItem
+                    key={product.name}
+                    primaryText={product.name}
+                    rightIconButton={
+                        <div>
+                            <IconButton>
+                                <DeleteIcon
+                                    onClick={() => this.deleteHandler(product.id)}
+                                />
+                            </IconButton>
+                            <IconButton
+                                onClick={() => this.isFavourite(product)}
+                            >
+                                {product.isFavourite === true ?
+                                    <ActionFavorite />
+                                    :
+                                    <ActionFavoriteBorder />
+                                }
+                            </IconButton>
+                        </div>
+                    }
+                />
+            ))
+
+        )
     }
 }
 
