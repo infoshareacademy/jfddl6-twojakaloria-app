ca5a3a034436cadea4dfacdb36f236146b20a22c
Krystian Pietroń <krystianpietr@gmail.com>
2018-12-04 19:36:14 +0100

    add imports and class in Auth.js

---
+++ src/views/Auth/Auth.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Paper from 'material-ui/Paper'
+import { auth, googleProvider } from '../../firebase'
+
+class Auth extends React.Component {
+    render() {
+        return (
+            <div>
+                Logowanie!!!
+            </div>
+        )
+    }
+}
