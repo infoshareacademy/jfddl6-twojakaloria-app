96d68de3932da3c6236373fa6ef81091c6f6d914
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-04 16:37:44 +0100

    Food components

--- src/components/AddFood.js
+++ src/components/AddFood.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Paper from 'material-ui/Paper'
 import { ListItem } from 'material-ui/List'
 import ActionFavorite from 'material-ui/svg-icons/action/favorite';
-import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
+import RaisedButton from 'material-ui/RaisedButton';
 import IconButton from 'material-ui/IconButton'
 import DeleteIcon from 'material-ui/svg-icons/action/delete'
 import DetailsIcon from 'material-ui/svg-icons/action/assignment'
@@ -12,9 +12,17 @@ const style = {
         margin: 30,
         padding: 30
     },
+    paperList: {
+        padding: 15,
+        marginTop: 10
+    },
     header: {
         margin: '0 auto',
         textAlign: 'center'
+    },
+    addButton: {
+        display: 'flex',
+        justifyContent: 'center'
     }
 }
 
@@ -30,123 +38,158 @@ class AddFood extends React.Component {
                     style={style.header}
                 >Create your daily plan!</h1>
 
-                <div>
+                <Paper
+                    style={style.paperList}
+                    zDepth={2}
+                >
                     <h2>Śniadanie</h2>
                     <ListItem
-                            key="c"
-                            primaryText="c"
-                            rightIconButton={
-                                <div>
-                                    <IconButton>
-                                        {/* <Link to={`/product/${product.id}`}> */}
-                                            <DetailsIcon />
-                                        {/* </Link> */}
-                                    </IconButton>
-                                    <IconButton>
-                                        <DeleteIcon
-                                            // onClick={(event) => {
-                                            //     event.preventDefault()
-                                            //     this.deleteTask(product)
-                                            // }
-                                            //}
-                                        />
-                                    </IconButton>
-                                    <IconButton
-                                        // onClick={(event) => {
-                                        //     event.preventDefault()
-                                        //     this.isFavorite(product)
-                                        // }
-                                        // }
-                                    >
-                                        {/* {product.isFavorite === true ? */}
-                                            <ActionFavorite />
-                                            {/* :
+                        key="c"
+                        primaryText="c"
+                        rightIconButton={
+                            <div>
+                                <IconButton>
+                                    {/* <Link to={`/product/${product.id}`}> */}
+                                    <DetailsIcon />
+                                    {/* </Link> */}
+                                </IconButton>
+                                <IconButton>
+                                    <DeleteIcon
+                                    // onClick={(event) => {
+                                    //     event.preventDefault()
+                                    //     this.deleteTask(product)
+                                    // }
+                                    //}
+                                    />
+                                </IconButton>
+                                <IconButton
+                                // onClick={(event) => {
+                                //     event.preventDefault()
+                                //     this.isFavorite(product)
+                                // }
+                                // }
+                                >
+                                    {/* {product.isFavorite === true ? */}
+                                    <ActionFavorite />
+                                    {/* :
                                             <ActionFavoriteBorder />
                                         } */}
-                                    </IconButton>
-                                </div>
-                            }
+                                </IconButton>
+                            </div>
+                        }
+                    />
+                    <div
+                        style={style.addButton}
+                    >
+                        <RaisedButton
+                            label="Add product!"
                         />
-                    <button>Dodaj produkt</button>
-                </div>
-                <div>
+                    </div>
+                </Paper>
+                <Paper
+                    style={style.paperList}
+                    zDepth={2}
+                >
                     <h2>Śniadanie</h2>
                     <ListItem
-                            key="c"
-                            primaryText="c"
-                            rightIconButton={
-                                <div>
-                                    <IconButton>
-                                        {/* <Link to={`/product/${product.id}`}> */}
-                                            <DetailsIcon />
-                                        {/* </Link> */}
-                                    </IconButton>
-                                    <IconButton>
-                                        <DeleteIcon
-                                            // onClick={(event) => {
-                                            //     event.preventDefault()
-                                            //     this.deleteTask(product)
-                                            // }
-                                            //}
-                                        />
-                                    </IconButton>
-                                    <IconButton
-                                        // onClick={(event) => {
-                                        //     event.preventDefault()
-                                        //     this.isFavorite(product)
-                                        // }
-                                        // }
-                                    >
-                                        {/* {product.isFavorite === true ? */}
-                                            <ActionFavorite />
-                                            {/* :
+                        key="c"
+                        primaryText="c"
+                        rightIconButton={
+                            <div>
+                                <IconButton>
+                                    {/* <Link to={`/product/${product.id}`}> */}
+                                    <DetailsIcon />
+                                    {/* </Link> */}
+                                </IconButton>
+                                <IconButton>
+                                    <DeleteIcon
+                                    // onClick={(event) => {
+                                    //     event.preventDefault()
+                                    //     this.deleteTask(product)
+                                    // }
+                                    //}
+                                    />
+                                </IconButton>
+                                <IconButton
+                                // onClick={(event) => {
+                                //     event.preventDefault()
+                                //     this.isFavorite(product)
+                                // }
+                                // }
+                                >
+                                    {/* {product.isFavorite === true ? */}
+                                    <ActionFavorite />
+                                    {/* :
                                             <ActionFavoriteBorder />
                                         } */}
-                                    </IconButton>
-                                </div>
-                            }
+                                </IconButton>
+                            </div>
+                        }
+                    />
+                    <div
+                        style={style.addButton}
+                    >
+                        <RaisedButton
+                            label="Add product!"
                         />
-                    <button>Dodaj produkt</button>
-                </div>
-                <div>
+                    </div>
+                </Paper>
+                <Paper
+                    style={style.paperList}
+                    zDepth={2}
+                >
                     <h2>Śniadanie</h2>
                     <ListItem
-                            key="c"
-                            primaryText="c"
-                            rightIconButton={
-                                <div>
-                                    <IconButton>
-                                        {/* <Link to={`/product/${product.id}`}> */}
-                                            <DetailsIcon />
-                                        {/* </Link> */}
-                                    </IconButton>
-                                    <IconButton>
-                                        <DeleteIcon
-                                            // onClick={(event) => {
-                                            //     event.preventDefault()
-                                            //     this.deleteTask(product)
-                                            // }
-                                            //}
-                                        />
-                                    </IconButton>
-                                    <IconButton
-                                        // onClick={(event) => {
-                                        //     event.preventDefault()
-                                        //     this.isFavorite(product)
-                                        // }
-                                        // }
-                                    >
-                                        {/* {product.isFavorite === true ? */}
-                                            <ActionFavorite />
-                                            {/* :
+                        key="c"
+                        primaryText="c"
+                        rightIconButton={
+                            <div>
+                                <IconButton>
+                                    {/* <Link to={`/product/${product.id}`}> */}
+                                    <DetailsIcon />
+                                    {/* </Link> */}
+                                </IconButton>
+                                <IconButton>
+                                    <DeleteIcon
+                                    // onClick={(event) => {
+                                    //     event.preventDefault()
+                                    //     this.deleteTask(product)
+                                    // }
+                                    //}
+                                    />
+                                </IconButton>
+                                <IconButton
+                                // onClick={(event) => {
+                                //     event.preventDefault()
+                                //     this.isFavorite(product)
+                                // }
+                                // }
+                                >
+                                    {/* {product.isFavorite === true ? */}
+                                    <ActionFavorite />
+                                    {/* :
                                             <ActionFavoriteBorder />
                                         } */}
-                                    </IconButton>
-                                </div>
-                            }
+                                </IconButton>
+                            </div>
+                        }
+                    />
+                    <div
+                        style={style.addButton}
+                    >
+                        <RaisedButton
+                            label="Add product!"
                         />
-                    <button>Dodaj produkt</button>
-                </div>
+                    </div>
+                </Paper>
+                <Paper
+                    style={style.paperList}
+                    zDepth={2}
+                >
+                    <div>
+                        Wykresy
+                    </div>
+                </Paper>
             </Paper>
         )
     }
